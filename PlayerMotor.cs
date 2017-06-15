e0c771c33a2eeac577acb90560925816d130185e
yusuf-95 <yusuf-95@live.com>
2017-06-15 12:19:26 +0100

    Add files via upload

--- PlayerMotor.cs
+++ PlayerMotor.cs
@@ -1,45 +1,101 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class PlayerMotor : MonoBehaviour {
 
     private CharacterController controller;
     private Vector3 moveVector;
 
-    private float speed = 10.0f;
+    private float speed = 5.0f;
+    private float jump = 4.0f;
     private float verticalVelocity = 0.0f;
     private float gravity = 12.0f;
-    public static bool TriggerHit = false;
-    public static bool ResetBarrel = false;
+
+    public Rigidbody PlayerRB;
+
+    public AudioClip[] PlayerSounds;
 
     // Use this for initialization
     void Start () {
 
         controller = GetComponent<CharacterController>();
-
+        PlayerRB = GetComponent<Rigidbody>();
+        moveVector = Vector3.zero;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        moveVector = Vector3.zero;
+        Move();
 
-        //if (controller.isGrounded)
-        //{
-        //    verticalVelocity = -0.5f;
-        //}
-        //else
-        //{
-        //    verticalVelocity -= gravity * Time.deltaTime;
-        //}
-        moveVector.x = Input.GetAxisRaw("Horizontal") * speed;
+        if (Enemy.GameOver == true)
+            Stop();   
+	}
 
-        //moveVector.y = verticalVelocity;
+    void Move()
+    {
+
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -gravity * Time.deltaTime;
+            if (Input.GetButtonDown("Jump"))
+            {
+                verticalVelocity = jump;
+                PlaySound(0);
+            }
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+        moveVector.x = Input.GetAxis("Horizontal") * speed;
+
+        moveVector.y = verticalVelocity;
 
         moveVector.z = speed;
 
-        controller.Move(Vector3.forward * speed * Time.deltaTime);
+        controller.Move(moveVector * Time.deltaTime);
 
-	}
+        if(speed != 0)
+        {
+            PlaySoundWalking(1);
+        }
+    }
 
+    void Stop()
+    {
+        speed = 0;
+        jump = 0;
+    }
+
+    void PlaySound(int clip)
+    {
+        AudioSource au = GetComponent<AudioSource>();
+        au.clip = PlayerSounds[clip];
+        au.Play();
+    }
+
+    void PlaySoundWalking(int clip)
+    {
+        AudioSource au = GetComponent<AudioSource>();
+        if (!au.isPlaying)
+        {
+            au.clip = PlayerSounds[clip];
+            au.Play();
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "stageEnd")
+        {
+            SceneManager.LoadScene("Stage2");
+        }
+
+        if(col.tag == "stageEnd2")
+        {
+            SceneManager.LoadScene("SplashScreen");
+        }
+    }
 }
