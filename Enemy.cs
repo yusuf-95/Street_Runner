0e473234bf1f04e02decb6e4c44f3d99eabf1e17
yusuf-95 <yusuf-95@live.com>
2017-06-15 12:18:37 +0100

    Add files via upload

---
+++ Enemy.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Enemy : MonoBehaviour
+{
+
+    public GameObject EnemyChase;
+    private Transform playerTransform;
+    public Animator Hit;
+    public int EnemySpeed;
+
+    public static bool GameOver = false;
+
+    public AudioClip[] EnemySounds;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        playerTransform = GameObject.FindGameObjectWithTag("PFP").transform;
+        Hit = GetComponent<Animator>();
+
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowPlayer();
+        if (GameOver == true)
+            Invoke("Load", 0.6f);
+    }
+
+    void FollowPlayer()
+    {
+        transform.LookAt(playerTransform);
+
+        //if (transform.position.z <= playerTransform.position.z - 10)
+        //    EnemySpeed = 8;
+        //if (transform.position.z < playerTransform.position.z - 1.5)
+        //    EnemySpeed = 5;
+
+        if (Vector3.Distance(transform.position, playerTransform.position) > 1.5)
+        {
+            transform.position += transform.forward * EnemySpeed * Time.deltaTime;
+        }
+        if (Vector3.Distance(transform.position, playerTransform.position) <= 1.5)
+        {
+            Hit.Play("Hit");
+            GameOver = true;
+            EnemySpeed = 0;
+            PlaySound(0);
+        }
+    }
+
+    void Load()
+    {
+            SceneManager.LoadScene("MainMenu");
+    }
+
+
+    void PlaySound(int clip)
+    {
+        AudioSource au = GetComponent<AudioSource>();
+        if (!au.isPlaying)
+        {
+            au.clip = EnemySounds[clip];
+            au.PlayDelayed(0.4f);
+        }
+    }
+
+}  
---
+++ SoundForObstacles.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundForObstacles : MonoBehaviour {
+
+    // Use this for initialization
+
+    public AudioClip[] ObSounds;
+
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (ObstacleMove.barrelRoll == true || ObstacleMove4.Barrel2Roll == true)
+        {
+            PlaySoundBarrel(1);
+        }
+
+        if (ObstacleMove2.Log2Drop == true || ObstacleMove3.Log3Drop == true || Obstacle1L2.Log1Drop == true)
+        {
+            PlaySoundLog(0);
+        }
+
+        if (Obstacle2L2.cageDrop == true)
+        {
+            PlaySoundcage(2);
+        }
+
+        if (Obstacle3L2.speared == true || Obstacle4L2.speared2 == true)
+        {
+            PlaySoundSpear(3);
+        }
+	
+	}
+
+    void PlaySoundLog(int clip)
+    {
+        AudioSource au = GetComponent<AudioSource>();
+        if (!au.isPlaying)
+        {
+            au.clip = ObSounds[clip];
+
+            if (Obstacle1L2.Log1Drop == true)
+            au.PlayDelayed(2f);
+
+            if (ObstacleMove2.Log2Drop == true)
+                au.PlayDelayed(1.2f);
+
+            if (ObstacleMove3.Log3Drop == true)
+                au.PlayDelayed(1.5f);
+
+            //au.Play();
+        }
+    }
+
+    void PlaySoundSpear(int clip)
+    {
+        AudioSource au = GetComponent<AudioSource>();
+        if (!au.isPlaying)
+        {
+            au.clip = ObSounds[clip];
+            au.PlayDelayed(0.1f);
+            au.volume = 0.5f;
+            //au.Play();
+        }
+    }
+
+    void PlaySoundcage(int clip)
+    {
+        AudioSource au = GetComponent<AudioSource>();
+        if (!au.isPlaying)
+        {
+            au.clip = ObSounds[clip];
+            au.PlayDelayed(0.2f);
+            //au.Play();
+        }
+    }
+
+
+    void PlaySoundBarrel(int clip)
+    {
+        AudioSource au = GetComponent<AudioSource>();
+        if (!au.isPlaying)
+        {
+            au.clip = ObSounds[clip];
+            au.PlayDelayed(0.0f);
+            //au.Play();
+        }
+    }
+}
